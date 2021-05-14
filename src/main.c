ccafce831a164b88fe1b20b126770e0b676bc7f8
Mahyar Koshkouei <mk@deltabeard.com>
2021-05-14 20:17:03 +0100

    ui: fix lvgl software rotation
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- src/main.c
+++ src/main.c
@@ -688,10 +688,12 @@ int main(int argc, char *argv[])
 	lv_disp_drv_top.user_data = ctx;
 #ifdef __3DS__
 	lv_disp_drv_top.rotated = LV_DISP_ROT_90;
-	lv_disp_drv_top.sw_rotate = 0;
+	lv_disp_drv_top.sw_rotate = 1;
         lv_disp_drv_top.hor_res = GSP_SCREEN_HEIGHT_TOP;
 	lv_disp_drv_top.ver_res = GSP_SCREEN_WIDTH_TOP;
 #else
+	lv_disp_drv_top.rotated = LV_DISP_ROT_90;
+	lv_disp_drv_top.sw_rotate = 1;
 	lv_disp_drv_top.hor_res = GSP_SCREEN_WIDTH_TOP;
 	lv_disp_drv_top.ver_res = GSP_SCREEN_HEIGHT_TOP;
 #endif
@@ -707,6 +709,8 @@ int main(int argc, char *argv[])
         lv_disp_drv_bot.hor_res = GSP_SCREEN_HEIGHT_BOT;
 	lv_disp_drv_bot.ver_res = GSP_SCREEN_WIDTH_BOT;
 #else
+	lv_disp_drv_bot.rotated = LV_DISP_ROT_90;
+	lv_disp_drv_bot.sw_rotate = 1;
 	lv_disp_drv_bot.hor_res = GSP_SCREEN_WIDTH_BOT;
 	lv_disp_drv_bot.ver_res = GSP_SCREEN_HEIGHT_BOT;
 #endif
